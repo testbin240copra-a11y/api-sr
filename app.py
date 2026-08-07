22318a9c223c1e9c5a53ce92fd14ee7e7229a852
testbin240copra-a11y <testbin240copra@gmail.com>
2026-08-07 08:38:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,11 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     import asyncio
-    import uvloop
     from aiohttp import web
     
     import aiohttp_app
     
     port = int(os.environ.get("PORT", 6667))
     
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     app = aiohttp_app.create_app()
-    web.run_app(app, host="0.0.0.0", port=port, access_log=None)
+    web.run_app(app, host="0.0.0.0", port=port)
